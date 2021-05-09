fca21944df7f4f160a0288d7b9b76b2676b6d72e
yanbe <y.yanbe@gmail.com>
2021-05-10 08:01:07 +0900

    support old env: CentOS 6 (kernel 2.6.32) / PX-W3PE / Docker 1.7

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:14.15.4-alpine
+RUN echo '@testing http://dl-cdn.alpinelinux.org/alpine/edge/testing/' >> /etc/apk/repositories
+RUN apk --update add ccid pcsc-tools@testing dbus udev openrc
+WORKDIR /app
+ENV DOCKER=YES
+ADD . .
+RUN apk add build-base && \
+    npm install && \
+    npm run build && \
+    npm install -g --unsafe-perm --production && \
+    cp -r /usr/local/lib/node_modules/mirakurun /app
+RUN rc-update add udev default && \
+    rc-update add dbus default && \
+    rc-update add pcscd default && \
+    mkdir /run/openrc
+ENTRYPOINT ["/bin/sh"]
+CMD ["./docker/container-init.sh"]
+EXPOSE 40772 9229
--- docker/container-init.sh
+++ docker/container-init.sh
@@ -23,14 +23,74 @@ fi
 
 # only for test purpose
 if !(type "arib-b25-stream-test" > /dev/null 2>&1); then
+  apk --update add pkgconfig pcsc-lite-dev 
   npm --prefix /opt install arib-b25-stream-test
   ln -sv /opt/node_modules/arib-b25-stream-test/bin/b25 /opt/bin/arib-b25-stream-test
 fi
 
+if !(type "recpt1" > /dev/null 2>&1); then
+  set -x
+  # install dependenices
+  apk add curl autoconf automake 
+  # get foltia's recpt1 and generate patch
+  mkdir /tmp/recpt1-original
+  curl -s http://aniloc.foltia.com/opensource/recpt1/{pt1-b14397800eae.tar.bz2} --output "/tmp/#1"
+  tar -jxf /tmp/pt1-b14397800eae.tar.bz2 --strip-components 1 --directory /tmp/recpt1-original
+  cp -r /tmp/recpt1-original /tmp/recpt1-foltia
+  curl -s http://aniloc.foltia.com/opensource/recpt1/{Makefile.in,pt1_dev.h,recpt1.h,recpt1.c} --output "/tmp/recpt1-foltia/recpt1/#1"
+  diff -u /tmp/recpt1-original/recpt1 /tmp/recpt1-foltia/recpt1 > /tmp/recpt1-foltia.patch 
+  
+  # generate patch for 2018 BS transponder migraiton
+  mkdir /tmp/recpt1-bsbase
+  curl -s http://hg.honeyplanet.jp/pt1/archive/{d56831676696.tar.bz2} --output "/tmp/pt1-#1"
+  tar -jxf /tmp/pt1-d56831676696.tar.bz2 --strip-components 1 --directory /tmp/recpt1-bsbase
+  mkdir /tmp/recpt1-bs2018
+  curl -s http://hg.honeyplanet.jp/pt1/archive/{17b4f7b5dccb.tar.bz2} --output "/tmp/pt1-#1"
+  tar -jxf /tmp/pt1-17b4f7b5dccb.tar.bz2 --strip-components 1 --directory /tmp/recpt1-bs2018
+  diff -u /tmp/recpt1-bsbase/recpt1/pt1_dev.h /tmp/recpt1-bs2018/recpt1/pt1_dev.h > /tmp/recpt1-bs2018.patch \
+    || perl -MEncode -pe 'Encode::from_to($_, "utf8", "eucjp");' /tmp/recpt1-bs2018.patch > /tmp/recpt1-bs2018-euc-jp.patch
+  
+  # get official pt1 with latest foltia compatible revision
+  mkdir /tmp/recpt1-latest
+  curl -s http://hg.honeyplanet.jp/pt1/archive/{61ff9cabf962.tar.bz2} --output "/tmp/pt1-#1"
+  tar -jxf /tmp/pt1-61ff9cabf962.tar.bz2 --strip-components 1 --directory /tmp/recpt1-latest
+  
+  # then patch them
+  cd /tmp/recpt1-latest/recpt1
+  patch -u < /tmp/recpt1-foltia.patch
+  patch -u < /tmp/recpt1-bs2018-euc-jp.patch
+  
+  # patch for musl environment
+  cd /tmp/recpt1-latest/recpt1
+  sed -i -E 's!(typedef struct )msgbuf!\1!' recpt1.c
+  sed -i -E 's!(typedef struct )msgbuf!\1!' recpt1ctl.c
+  sed -i -E 's!(#include <sys/stat.h>)!\1\n#include <sys/types.h>!' tssplitter_lite.c
+  
+  # get as5220 driver from plex
+  curl -s http://plex-net.co.jp/download/linux/{Linux_Driver.zip} --output "/tmp/#1"
+  unzip -q /tmp/Linux_Driver.zip -d /tmp
+  cp /tmp/Linux_Driver/MyRecpt1/MyRecpt1/recpt1/asicen_dtv.c /tmp/Linux_Driver/MyRecpt1/MyRecpt1/recpt1/asicen_dtv.h /tmp/recpt1-latest/recpt1
+  
+  # build recpt1 for PX-W3PE dtv tuners
+  cd /tmp/recpt1-latest/recpt1
+  ./autogen.sh 
+  ./configure --prefix /opt 
+  make
+  make install
+  cd /app
+fi
+
 if [ -e "/etc/init.d/pcscd" ]; then
+  echo "Servece 'All': Status"
+  rc-status -a
+  touch /run/openrc/softlevel
+  echo "starting udev..."
+  rc-service udev restart
+  echo "starting dbus..."
+  rc-service dbus restart
+  echo "starting pcscd..."
+  rc-service pcscd restart
   while :; do
-    echo "starting pcscd..."
-    /etc/init.d/pcscd start
     sleep 1
     timeout 2 pcsc_scan | grep -A 50 "Using reader plug'n play mechanism"
     if [ $? = 0 ]; then
@@ -48,6 +108,12 @@ fi
 
 if [ -e "/etc/init.d/pcscd" ]; then
   echo "stopping pcscd..."
-  /etc/init.d/pcscd stop
+  rc-service pcscd stop
+  echo "stoping dbus..."
+  rc-service dbus stop
+  echo "stoping udev..."
+  rc-service udev stop
   sleep 1
+  echo "Servece 'All': Status"
+  rc-status -a
 fi
